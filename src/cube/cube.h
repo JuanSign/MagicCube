2034de5e05e5a10e6dcadd4bafdb764399f8034b
Juan Siagian <jsa.siagian@gmail.com>
2024-11-07 00:13:30 +0700

    changing CUBE's attributes access modifier

--- src/cube/cube.h
+++ src/cube/cube.h
@@ -10,14 +10,15 @@ class CUBE
 {
 private:
     array<array<array<int, 5>, 5>, 5> DATA; // literal cube
-    array<array<int, 3>, 125> POSITIONS;    // the (i,j,k)-coordinate of each number
-    array<array<int, 5>, 109> CONSTRAINTS;  // the 109 magic cube's constraints
-    array<vector<int>, 125> ADJ;            // the adjacency list of the constraint graph
 
     void GENERATE_POSITIONS();   // calculate the positions of each number
     void GENERATE_CONSTRAINTS(); // generate the magic cube's contraints
 
 public:
+    array<array<int, 3>, 125> POSITIONS;   // the (i,j,k)-coordinate of each number
+    array<array<int, 5>, 109> CONSTRAINTS; // the 109 magic cube's constraints
+    array<vector<int>, 125> ADJ;           // the adjacency list of the constraint graph
+
     CUBE(int seed = 0);      // instantiate a random cube (0 or no seed for true random, seed != 0 for pseudo-random)
     void SWAP(int i, int j); // swap number i with number j
     void PRINT_CUBE();       // output the cube to stdout
