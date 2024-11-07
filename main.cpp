a14080ea341f7b0d12d8d6a6e4766a0026e84b09
Juan Siagian <jsa.siagian@gmail.com>
2024-11-07 17:01:25 +0700

    adding guide

--- main.cpp
+++ main.cpp
@@ -3,4 +3,17 @@
 
 int main()
 {
+    // CUBE
+    // CUBE c; // instantiate a true-random cube
+    // CUBE c(i); // for an integer i != 0, instantiate a pseudo-random cube with seed 0
+
+    // HCSA
+    /*
+        c is a CUBE
+        i is the choosen objective function (default is 0)
+        <variable_name> is the prefered name of the HCSA instance
+    */
+    // HCSA <variable_name>(c,i);
+    // <variable_name>.SET_OPTION(i); // set the objective function to option i
+    // <variable_name>.CALCULATE(c); // calculate the objective function of CUBE c
 }
